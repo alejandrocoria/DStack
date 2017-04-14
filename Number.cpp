043940f1e86120fb683ae7a64c58e242e94edf4d
Dennis <dennis@tryitonline.net>
2017-04-14 01:42:07 +0000

    added cast to bool (required by gcc 6)

--- Number.cpp
+++ Number.cpp
@@ -52,7 +52,7 @@ Number readNumber(std::istream &stream){
     do{
         std::getline(stream, tmp);
         std::stringstream ss(tmp);
-        exit = ss >> number;
+        exit = (bool) ss >> number;
     } while (!exit);
 
     return number;
